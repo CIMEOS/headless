b65333d0e215eedfbe54ddbbfcf9de51810f39f7
esteban <esteban.gagneur@cameros.fr>
2024-11-26 08:38:29 +0100

    [TASK] CI/CD code quality check

--- Classes/XClass/Controller/PasswordRecoveryController.php
+++ Classes/XClass/Controller/PasswordRecoveryController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the "headless" Extension for TYPO3 CMS.
+ * This file is part of the 'headless' Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.md file that was distributed with this source code.
@@ -11,18 +11,13 @@ declare(strict_types=1);
 
 namespace FriendsOfTYPO3\Headless\XClass\Controller;
 
-use TYPO3\CMS\Extbase\Http\ForwardResponse;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Mvc\Exception\StopActionException;
-use TYPO3\CMS\Core\Messaging\AbstractMessage;
-use CIMEOS\CimUsers\Domain\Repository\UserRepository;
-use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
-use TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashFactory;
 use FriendsOfTYPO3\Headless\Utility\HeadlessMode;
+use TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Http\ForwardResponse;
 
 class PasswordRecoveryController extends TYPO3\CMS\FrontendLogin\Controller\PasswordRecoveryController
 {
-
     /**
      * Shows the recovery form. If $userIdentifier is set, an email will be sent, if the corresponding user exists and
      * has a valid email address set.
@@ -92,57 +87,56 @@ class PasswordRecoveryController extends TYPO3\CMS\FrontendLogin\Controller\Pass
     protected function buildShowForm($hash)
     {
         return [
-            "id" => "felogin-recovery",
+            'id' => 'felogin-recovery',
             'method' => 'POST',
-            "action" => $this->uriBuilder
+            'action' => $this->uriBuilder
                 ->reset()
                 ->setTargetPageUid($GLOBALS['TSFE']->id)
                 ->uriFor('changePassword'),
-            "api" => [
-                "label" => 'Forget password'
+            'api' => [
+                'label' => 'Forget password'
             ],
-            "elements" => [
+            'elements' => [
                 [
-                    "identifier" => "newPass",
-                    "type" => "Password",
-                    "label" => "Nouveau mot de passe",
-                    "name" => 'tx_felogin_login[newPass]',
-                    "value" => '',
-                    "defaultValue" => '',
-                    "validators" => [
+                    'identifier' => 'newPass',
+                    'type' => 'Password',
+                    'label' => 'Nouveau mot de passe',
+                    'name' => 'tx_felogin_login[newPass]',
+                    'value' => '',
+                    'defaultValue' => '',
+                    'validators' => [
                         [
-                            "identifier" => "required",
-                            "message" => "Le titre est obligatoire."
+                            'identifier' => 'required',
+                            'message' => 'Le titre est obligatoire.'
                         ]
                     ],
                 ],
                 [
-                    "identifier" => "newPassRepeat",
-                    "type" => "Password",
-                    "label" => "Répeter le nouveau mot de passe",
-                    "name" => 'tx_felogin_login[newPassRepeat]',
-                    "value" => '',
-                    "defaultValue" => '',
-                    "validators" => [
+                    'identifier' => 'newPassRepeat',
+                    'type' => 'Password',
+                    'label' => 'Répeter le nouveau mot de passe',
+                    'name' => 'tx_felogin_login[newPassRepeat]',
+                    'value' => '',
+                    'defaultValue' => '',
+                    'validators' => [
                         [
-                            "identifier" => "required",
-                            "message" => "Le titre est obligatoire."
+                            'identifier' => 'required',
+                            'message' => 'Le titre est obligatoire.'
                         ]
                     ],
                 ],
                 [
-                    "identifier" => "hash",
-                    "type" => "Hidden",
-                    "label" => "Hash",
-                    "name" => 'tx_felogin_login[hash]',
-                    "value" => $hash,
-                    "defaultValue" => '',
+                    'identifier' => 'hash',
+                    'type' => 'Hidden',
+                    'label' => 'Hash',
+                    'name' => 'tx_felogin_login[hash]',
+                    'value' => $hash,
+                    'defaultValue' => '',
                 ],
 
             ]
         ];
     }
-
 
     public function changePasswordAction(string $newPass, string $hash): \Psr\Http\Message\ResponseInterface
     {
@@ -161,11 +155,10 @@ class PasswordRecoveryController extends TYPO3\CMS\FrontendLogin\Controller\Pass
             ->getHashedPassword($newPass);
 
         if (($hashedPassword = $this->notifyPasswordChange(
-                $newPass,
-                $hashedPassword,
-                $hash
-            )) instanceof ForwardResponse) {
-
+            $newPass,
+            $hashedPassword,
+            $hash
+        )) instanceof ForwardResponse) {
             return $this->jsonResponse();
         }
 
